d7d9a99bfbeef5405687a3c4db85e11b944c7029
Fábio José Mesquita <fjmeskta@gmail.com>
2021-05-16 15:59:04 -0300

    Transactions Table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Sumary";
+import { TransactionsTable } from "../TrasactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 }
